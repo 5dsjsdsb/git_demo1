d520d64ca0ec243c8a74cfba7a1bb451a39583a5
dsjsdsb <d17623438786@163.com>
2025-04-12 16:26:58 +0800

    添加的方法

---
+++ AddMethod.java
@@ -0,0 +1,10 @@
+public class AddMethod(){
+	public static void main(String[] args){
+		int x = 10;
+		int y = 20;
+		System.out.println(add(x,y));
+	}
+	public int add(int x, int y){
+		return x+y;
+	}
+}
